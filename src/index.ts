0bb1752171b26e031e81ac91b6fd2f5bcb1f0c93
JuanPyC <juan5080ttt@gmail.com>
2026-06-04 13:46:29 -0500

    Exponer endpoints REST de contenedores (bins) bajo /api/v1/bins

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import { authRouter } from "./interface/routes/auth";
 import { localRouter } from "./interface/routes/local";
 import { redemptionsRouter } from "./interface/routes/redemptions";
 import { quizzesRouter } from "./interface/routes/quizzes";
+import { binsRouter } from "./interface/routes/bins";
 
 dotenv.config();
 
@@ -215,6 +216,7 @@ app.use("/api/v1/news", newsRouter);
 app.use("/api/v1/profiles", profilesRouter);
 app.use("/api/v1/redemptions", redemptionsRouter);
 app.use("/api/v1/quizzes", quizzesRouter);
+app.use("/api/v1/bins", binsRouter);
 app.use("/api/v1/local", localRouter);
 
 // Root
@@ -232,6 +234,7 @@ app.get("/", (_req, res) => {
       "GET /api/v1/profiles",
       "GET /api/v1/redemptions",
       "GET /api/v1/quizzes",
+      "GET /api/v1/bins/qr?qr_code=...",
     ],
   });
 });
